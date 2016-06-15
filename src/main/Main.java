ed3fc6fcf12b8f5595712fee1f62f3cb78873578
MilosMisic <mybadmc@hotmail.com>
2016-06-15 16:16:02 +0200

    changed output

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,16 @@ public class Main {
 		Weather w = new Weather();
 		Wind wind = new Wind();
 
-		System.out.println(w.getTemp() + "\n" + w.getHumidity() + "\n" + wind.getWindSpeed() + "\n" + wind.getDegree());
+		StringBuilder output = new StringBuilder();
+		output.append(w.getTemp());
+		output.append("\n");
+		output.append(w.getHumidity());
+		output.append("\n");
+		output.append(wind.getWindSpeed());
+		output.append("\n");
+		output.append(wind.getDegree());
+	
+		System.out.println(output);
 
 	}
 
